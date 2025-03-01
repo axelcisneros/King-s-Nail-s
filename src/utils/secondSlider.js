2c91c269fe4fc8343d1df98b49de6de41569017c
Axel Cisneros <axel.cisneros25@hotmail.com>
2025-02-28 19:03:06 -0600

    fixed route img

--- src/utils/secondSlider.js
+++ src/utils/secondSlider.js
@@ -1,27 +1,27 @@
 const secondSlider = [
     {
         id: "1",
-        link: "https://scontent.fmex1-6.fna.fbcdn.net/v/t39.30808-6/412026594_374613268489005_1659612752276570918_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=833d8c&_nc_ohc=JBxkfLJ5MVoQ7kNvgHhxgX2&_nc_oc=AdjFTFtHmZ738XHGXbc8ET-_UM0J_uLiHPW3RsSmKfJM3FEcr5dVW0yz5kOoXUSrOku0osD6jBew7odsI0ht4HNB&_nc_zt=23&_nc_ht=scontent.fmex1-6.fna&_nc_gid=Av5jbMrgbC-S3mbBJnm1ybh&oh=00_AYCkFAKENTgtemlBdOQYV5qa6JjsiYFhHYQ6CE0AZs8XEQ&oe=67B026E3"
+        link: "src/assets/fotos/works69.jpg"
     },
 
     {
         id: "2",
-        link: "https://scontent.fmex1-2.fna.fbcdn.net/v/t39.30808-6/411834665_374613901822275_9152355587106821465_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_ohc=bKCm-ezmnWUQ7kNvgHAggnG&_nc_oc=AdixXD8B9y1-hboCKjvkefiV2wLuznGXt4f_yGaQiIM08J2Dm397eXct2DTV6-Z7rDq8Wzo53EPg7Vm-W1bjkpuf&_nc_zt=23&_nc_ht=scontent.fmex1-2.fna&_nc_gid=APxS5bk0aXtE7JTXaN-4ejs&oh=00_AYBDwsgoe71tA_IZyWQiQeOC8UAGoNMv1Ajvs07Wbqdp4g&oe=67B00DA5"
+        link: "src/assets/fotos/works65.jpg"
     },
 
     {
         id: "3",
-        link: "https://scontent.fmex1-5.fna.fbcdn.net/v/t39.30808-6/411713426_374613405155658_6576431738573550799_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=833d8c&_nc_ohc=dt8CQNGWrzgQ7kNvgHwqQpf&_nc_oc=AdiLznzBFCupeNzNAMXzPyhEPajqSU3iYsTm4vl1cQ-R1ZB4PDL81SNFphm42ovPPMmxCtRMqVB5241_FtsV-_UE&_nc_zt=23&_nc_ht=scontent.fmex1-5.fna&_nc_gid=AnI2KLMYqqIMu8gWkLhF3HP&oh=00_AYBZyug2wo9dk5A9zsbvNcUs-EeUvQzPbQHRbAEQn2IduA&oe=67B01D94"
+        link: "src/assets/fotos/works66.jpg"
     },
 
     {
         id: "4",
-        link: "https://scontent.fmex1-5.fna.fbcdn.net/v/t39.30808-6/402039055_358325380117794_8407677923672675696_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_ohc=meksgOAY_7YQ7kNvgHEXwsq&_nc_oc=AdhbCBcQIcgeqhIJCco2VkEUFfZzmyTFXdaGdguPmGkg3B6dvRjXhJ1eI8d4W4HggVFzt0lHUcEvwdoOcUTjY2hK&_nc_zt=23&_nc_ht=scontent.fmex1-5.fna&_nc_gid=AcLCb8ovQMC_JHGc_ncnfF0&oh=00_AYDTl0dRA8fsNB9S0QJsBiSd2W5pT4Zkd2r1wkQ13EjOnw&oe=67B0215E"
+        link: "src/assets/fotos/works67.jpg"
     },
 
     {
         id: "5",
-        link: "https://scontent.fmex1-6.fna.fbcdn.net/v/t39.30808-6/402621450_358325213451144_231758690821512298_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_ohc=DzbpbuYcWF0Q7kNvgGJ3Ave&_nc_oc=Adiq2T-pS3bo8la2Q7yy6axdqmSSWWZgzZ8pLXGF8jfx6MWUM8Ea5dYLjnzerLNzWH7Qk9yQoiiOOeWAOCotYXf2&_nc_zt=23&_nc_ht=scontent.fmex1-6.fna&_nc_gid=AkE7fL_F3ueMXTz0drVzk8_&oh=00_AYCzd_QzhNzrmfMybVKEsKfsO7lemf1JjiGDLlftH7BAwA&oe=67B001C6"
+        link: "src/assets/fotos/works68.jpg"
     }
 ]
 
--- src/utils/sliderConfig.js
+++ src/utils/sliderConfig.js
@@ -1,27 +1,27 @@
 const sliderConfig = [
     {
         id: "1",
-        link: "https://scontent.fmex1-6.fna.fbcdn.net/v/t39.30808-6/447956081_474864431797221_315564997621607515_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=f727a1&_nc_ohc=QxqMstCo0RgQ7kNvgHrYI5Y&_nc_oc=AdgYgFuqpSoXFvTvqWEXvvtTZmtDJVQUQbX8eUmbtZXR_APfFQjt-waFcYggjEPj9uuNqJ7CFLgj1PJJlbmJj7qM&_nc_zt=23&_nc_ht=scontent.fmex1-6.fna&_nc_gid=AJtqw9_Pjkf4E8UlXRwKG2d&oh=00_AYBoUY_csPyurp5wE5OwC2efSPTIC7zhbPc5NCkluocy3A&oe=67B58202"
+        link: "src/assets/fotos/Kit-inicio.jpg"
     },
 
     {
         id: "2",
-        link: "https://scontent.fmex1-2.fna.fbcdn.net/v/t39.30808-6/444776717_469266865690311_5680308550734900115_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_ohc=L3ZIlNkMoHgQ7kNvgELjaqd&_nc_oc=AdisrejfGKRd5Kd6rywRRXzA7g_VXf4Ajl372qwpvkeXhgarp-0DJzvHAcFT58kFFdJFcqNfkl4-J8fo-3BZ7dj0&_nc_zt=23&_nc_ht=scontent.fmex1-2.fna&_nc_gid=A6jq3no6NtVYO-Ky1ohZLqd&oh=00_AYDGgh5wmNrBjkGvCAXSji7YKiblO3yOwJq1YEYrrfjZAg&oe=67B5AE49"
+        link: "src/assets/fotos/works64.jpg"
     },
 
     {
         id: "3",
-        link: "https://scontent.fmex1-1.fna.fbcdn.net/v/t39.30808-6/441867839_469264182357246_8825408981517992979_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_ohc=m6drqW907-YQ7kNvgHHqIUt&_nc_oc=Adj4IyF__MfJyBne9Rz89zpK_ui3uLI1hb1Avdsv1sbgSqS2pIcjNZ5dLW8GFe1f51qi0D7vyLMaBqxujvoK1ZoT&_nc_zt=23&_nc_ht=scontent.fmex1-1.fna&_nc_gid=ASVV62RjxVCVP4U7iIKwkBE&oh=00_AYBoXt3h79P3f1Bn5z5GUXfvtZHIotZUOKyMKrAKKSV91Q&oe=67B5A4C5"
+        link: "src/assets/fotos/works71.jpg"
     },
 
     {
         id: "4",
-        link: "https://scontent.fmex1-1.fna.fbcdn.net/v/t39.30808-6/410283001_374855108464821_788082992180108220_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_ohc=2zkr3iHsFYYQ7kNvgF346LH&_nc_oc=AdijCyvP9NIAqjqFLnVnslynz0JrXr8VoYsBqXWBlC1ssHzGkMvitA3AHOqexpcJWx_z1m68B7bLRkm_ec1-H6QD&_nc_zt=23&_nc_ht=scontent.fmex1-1.fna&_nc_gid=AxivSbJzpN8ogX3VJiMiEg_&oh=00_AYA7jwalUlgbJAmrW3rUOGBQHX-rEMp2r9T6xi_hnindZQ&oe=67B58F1E"
+        link: "src/assets/fotos/works70.jpg"
     },
 
     {
         id: "5",
-        link: "https://scontent.fmex1-1.fna.fbcdn.net/v/t39.30808-6/412035038_374614898488842_3440042466270650703_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_ohc=RmiBAaEAo3YQ7kNvgEqhGdl&_nc_oc=AdhWF-bX5vuu8VFVlc8LAa8k5keZ2AajdAgfav19IDVLWamlUiHHLkxPXtV33rhfmMhE_Jda3ssIqBDCHP9Tj_VE&_nc_zt=23&_nc_ht=scontent.fmex1-1.fna&_nc_gid=A8jzM2gCsSnSs9DXJUo0vgz&oh=00_AYCby-j6YwI3SehOxJmEieeANV8lBupt7R0odXtCrhLB1w&oe=67B5901D"
+        link: "src/assets/fotos/works72.jpg"
     }
 ]
 
