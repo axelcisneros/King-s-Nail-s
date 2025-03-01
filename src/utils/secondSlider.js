597a997d480d33e4cc5cd2b25baca2caa2e033ce
Axel Cisneros <axel.cisneros25@hotmail.com>
2025-02-28 22:55:19 -0600

    fixed bugs

--- src/utils/secondSlider.js
+++ src/utils/secondSlider.js
@@ -1,8 +1,8 @@
-import works69 from "@assets/fotos/works69";
-import works65 from "@assets/fotos/works65";
-import works66 from "@assets/fotos/works66";
-import works67 from "@assets/fotos/works67";
-import works68 from "@assets/fotos/works68";
+import works69 from "@assets/fotos/works69.jpg";
+import works65 from "@assets/fotos/works65.jpg";
+import works66 from "@assets/fotos/works66.jpg";
+import works67 from "@assets/fotos/works67.jpg";
+import works68 from "@assets/fotos/works68.jpg";
 
 const secondSlider = [
     {
