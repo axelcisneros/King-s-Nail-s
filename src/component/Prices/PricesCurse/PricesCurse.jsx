cbcf5a68ae3ca4764963465036119e115b911d80
Axel Cisneros <axel.cisneros25@hotmail.com>
2025-02-28 19:52:07 -0600

    fixed bugs

--- src/component/Prices/PricesCurse/PricesCurse.jsx
+++ src/component/Prices/PricesCurse/PricesCurse.jsx
@@ -1,17 +1,17 @@
 function PricesCurse() {
     return (
       <div className="prices__curse">
-        <p className="prices__paragraph">
-          Cursos en línea**
-        </p>
-        <p className="prices__paragraph">
-          $150.00
-        </p>
-        <img
-        className="prices__image"
-        src='../../../assets/fotos/Kit-inicio.jpg'
-        alt=""
-        />
+      <p className="prices__paragraph">
+        Cursos en línea**
+      </p>
+      <p className="prices__paragraph">
+        $150.00
+      </p>
+      <img
+      className="prices__image"
+      src='/public/fotos/Kit-inicio.jpg'
+      alt=""
+      />
       </div>
     )
 }
