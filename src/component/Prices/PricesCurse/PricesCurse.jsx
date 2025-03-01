6bca8eea81d51318532b96f27443aec7c2abceb6
Axel Cisneros <axel.cisneros25@hotmail.com>
2025-02-28 19:29:00 -0600

    fixed bugs

--- src/component/Prices/PricesCurse/PricesCurse.jsx
+++ src/component/Prices/PricesCurse/PricesCurse.jsx
@@ -9,7 +9,7 @@ function PricesCurse() {
             </p>
             <img
             className="prices__image"
-            src="https://scontent.fmex1-6.fna.fbcdn.net/v/t39.30808-6/447956081_474864431797221_315564997621607515_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=f727a1&_nc_ohc=QxqMstCo0RgQ7kNvgHrYI5Y&_nc_oc=AdgYgFuqpSoXFvTvqWEXvvtTZmtDJVQUQbX8eUmbtZXR_APfFQjt-waFcYggjEPj9uuNqJ7CFLgj1PJJlbmJj7qM&_nc_zt=23&_nc_ht=scontent.fmex1-6.fna&_nc_gid=AJtqw9_Pjkf4E8UlXRwKG2d&oh=00_AYBoUY_csPyurp5wE5OwC2efSPTIC7zhbPc5NCkluocy3A&oe=67B58202"
+            src="./src/assets/fotos/Kit-inicio.jpg"
             alt=""
             />
         </div>
