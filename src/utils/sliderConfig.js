6291971050335850848df85c0168577212156f7e
Axel Cisneros <axel.cisneros25@hotmail.com>
2025-02-28 22:27:43 -0600

    fixed bugs

--- src/utils/sliderConfig.js
+++ src/utils/sliderConfig.js
@@ -1,7 +1,13 @@
+import kit from "@assets/images/Kit-inicio.jpg";
+import works64 from "@assets/images/works64.jpg";
+import works71 from "@assets/images/works71.jpg";
+import works70 from "@assets/images/works70.jpg";
+import works72 from "@assets/images/works72.jpg";
+
 const sliderConfig = [
     {
         id: "1",
-        link: "@assets/images/Kit-inicio.jpg"
+        link: { kit }
     },
 
     {
