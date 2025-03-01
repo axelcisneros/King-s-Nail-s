110ea10005b4625cf3a5987f041ce1cf5ed2506f
Axel Cisneros <axel.cisneros25@hotmail.com>
2025-02-28 22:20:02 -0600

    fixed bugs

--- src/utils/sliderConfig.js
+++ src/utils/sliderConfig.js
@@ -1,27 +1,27 @@
 const sliderConfig = [
     {
         id: "1",
-        link: "../src/assets/images/Kit-inicio.jpg"
+        link: "@assets/images/Kit-inicio.jpg"
     },
 
     {
         id: "2",
-        link: "../src/assets/images/works64.jpg"
+        link: "@assets/images/works64.jpg"
     },
 
     {
         id: "3",
-        link: "../src/assets/images/works71.jpg"
+        link: "@assets/images/works71.jpg"
     },
 
     {
         id: "4",
-        link: "../src/assets/images/works70.jpg"
+        link: "@assets/images/works70.jpg"
     },
 
     {
         id: "5",
-        link: "../src/assets/images/works72.jpg"
+        link: "@assets/images/works72.jpg"
     }
 ]
 
